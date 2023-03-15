bc680efe67c6e36d34069f7dc132b19b284fd6ab
mmti <mmshooreshi@gmail.com>
2023-03-16 03:29:45 +0330

    s

--- main2.py
+++ main2.py
@@ -1,10 +1,19 @@
+# -*- coding: utf-8 -*-
+
 import logging
+
 import telegram, os
 from flask import Flask, request
 from telegram.ext import Dispatcher, MessageHandler, Filters, CommandHandler
+
+
+
+#################
 import openai
 	
 openai.api_key = os.getenv("OPENAI_API_KEY") 
+
+
 chat_language = os.getenv("INIT_LANGUAGE", default = "fa") 
 	
 MSG_LIST_LIMIT = int(os.getenv("MSG_LIST_LIMIT", default = 20))
@@ -15,7 +24,8 @@ LANGUAGE_TABLE = {
       "fa": "درود!"
 	}
 
-context = "you are helpful assistant"
+context = "JUST OUTPUT GANTT CHART FORMAT | This Gantt chart provides a detailed and tidy plan for the purpose described in the [AI_EXTRA] and related specific practical tasks in the following. The plan is not a general timeplan. its specific at it most detailed version it can be. The plan is designed to cover the most important aspects of Vue.js in a step-by-step manner, with each task building on the previous one. Just send output as mermaid in markdown without code blocks or backticks, without anything else. This is an example working nicely, make sure to send output just in this format styling:"
+
 
 class Prompts:
     def __init__(self):
@@ -26,7 +36,9 @@ class Prompts:
         ]
 
         self.messagesTk= [50,0]
+
         self.msg_list.append(f"AI:{LANGUAGE_TABLE[chat_language]}")
+	    
     def add_msg(self, new_msg):
         if len(self.messages) >= MSG_LIST_LIMIT:
             self.remove_msg()
@@ -43,68 +55,100 @@ class Prompts:
         self.messages.append({"role": role, "content": content})
         
         if role == "assistant" and usage is not None:
-            added_tokens = usage['total_tokens'] - self.messagesTk[-1]
-            self.messagesTk.append(added_tokens)
+            addedTokens = usage['total_tokens'] - self.messagesTk[-1]
+            self.messagesTk.append(addedTokens)
         else:
             self.messagesTk.append(0)
-   
+
     def shorten(self,prompt_tokens, completion_tokens, total_tokens):
-        excessive_tokens_count = total_tokens - 4096
-        indices_to_remove = []
+        excessive_tokens_count= total_tokens-4096
+        indice_to_remove=[]
         for index, value in enumerate(self.messages):
-            if excessive_tokens_count == 0:
+            if(excessive_tokens_count==0):
                 break
-            if index == 0:
+            if(index ==0):
                 pass
             else:
-                if self.messagesTk[index] / excessive_tokens_count < 1.1:
-                    indices_to_remove.append(index)
+                if(self.messagesTk[index]/excessive_tokens_count < 1.1):
+                    indice_to_remove.append(index)
                     excessive_tokens_count -= self.messagesTk[index]
                 else:
-                    truncating_position = (self.messagesTk[index] / excessive_tokens_count) * 1.3 * len(self.messages[index]['content'])
+                    truncating_position = (self.messagesTk[index] / excessive_tokens_count)*1.3*len(self.messages[index]['content'])
                     excessive_tokens_count = 0
-                    self.messages[index]['content'] = self.messages[index]['content'][:truncating_position]
-        if len(indices_to_remove):
-            for i, value in enumerate(indices_to_remove):
+                    self.messages[index]['content'] = self.messages[index]['content'][truncating_position:]
+        if(len(indice_to_remove)):
+             for i, value in enumerate(indice_to_remove):
                 self.messages.pop(value)
                 self.messagesTk.pop(value)
 
 class ChatGPT:  
     def __init__(self):
         self.prompt = Prompts()
-        self.model = os.getenv("OPENAI_MODEL", default = "gpt-3.5-turbo-0301")
+        self.model = os.getenv("OPENAI_MODEL", default = "text-davinci-003")
         self.temperature = float(os.getenv("OPENAI_TEMPERATURE", default = 0.5))
         self.frequency_penalty = float(os.getenv("OPENAI_FREQUENCY_PENALTY", default = 0))
         self.presence_penalty = float(os.getenv("OPENAI_PRESENCE_PENALTY", default = 0))
         self.max_tokens = int(os.getenv("OPENAI_MAX_TOKENS", default = 250))
 	
     def get_response(self):
-        while_override = 0
-        while not while_override:
+
+        whileOverride=0
+        while(not whileOverride):
             response = openai.ChatCompletion.create(
-                model=self.model,
-                messages=self.prompt.messages,
-                temperature=self.temperature,
-                frequency_penalty=self.frequency_penalty,
-                presence_penalty=self.presence_penalty,
-                max_tokens=self.max_tokens
-            )
+	                model="gpt-3.5-turbo-0301",
+                    messages=self.prompt.messages,
+	                temperature=self.temperature,
+	                frequency_penalty=self.frequency_penalty,
+	                presence_penalty=self.presence_penalty,
+	                max_tokens=self.max_tokens
+                    )
+        
             usage = response['usage']
             content = response['choices'][0]['message']['content']
 
-            if usage['total_tokens'] > 4090:
-                self.prompt.shorten(usage['prompt_tokens'], usage['completion_tokens'], usage['total_tokens'])
+            if(usage['total_tokens']>4090):
+                self.prompt.shorten(usage['prompt_tokens'],usage['completion_tokens'],usage['total_tokens'])
             else:
-                while_override = 1
+                whileOverride=1
 
-        self.prompt.update_messages("assistant", content, usage)
+        self.prompt.update_messages("assistant",content,usage)
+        
+        # response = openai.Completion.create(
+	    #         model=self.model,
+	    #         prompt=self.prompt.generate_prompt(),
+	    #         temperature=self.temperature,
+	    #         frequency_penalty=self.frequency_penalty,
+	    #         presence_penalty=self.presence_penalty,
+	    #         max_tokens=self.max_tokens
+        #         )
+        
+        print("ربات")        
+        print(response['choices'][0]['message']['content'].strip())
 
+        print("ربات : ")      
+        print(response)
+        print("\n \n \n",f"{len(self.prompt.messages)}","\n \n \n")
+        print("self.prompt.messages: \n \n \n",self.prompt.messages)
+        
         return response['choices'][0]['message']['content'].strip()
-
+	
     def add_msg(self, text):
         self.prompt.add_msg(text)
 
-telegram_bot_token = os.getenv("TELEGRAM_BOT_TOKEN")
+
+
+
+
+
+#####################
+
+telegram_bot_token = str(os.getenv("TELEGRAM_BOT_TOKEN"))
+
+
+
+# Load data from config.ini file
+#config = configparser.ConfigParser()
+#config.read('config.ini')
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -116,7 +160,9 @@ app = Flask(__name__)
 
 # Initial bot by Telegram access token
 bot = telegram.Bot(token=telegram_bot_token)
-chatgpt = ChatGPT()
+chatgpt = ChatGPT()        
+
+
 
 @app.route('/callback', methods=['POST'])
 def webhook_handler():
@@ -128,41 +174,44 @@ def webhook_handler():
         dispatcher.process_update(update)
     return 'ok'
 
+
 def reply_handler(bot, update):
     """Reply message."""
+    #text = update.message.text
+    #update.message.reply_text(text)
+    
     bot.send_chat_action(chat_id=update.message.chat_id, action=telegram.ChatAction.TYPING)
-    chatgpt.prompt.add_msg(update.message.text)
-    chatgpt.prompt.update_messages("user", update.message.text)
+    chatgpt.prompt.add_msg(update.message.text) #人類的問題 the question humans asked
+    chatgpt.prompt.update_messages("user",update.message.text)
 
-    ai_reply_response = chatgpt.get_response()
-
-    update.message.reply_text(ai_reply_response)
-
+    ai_reply_response = chatgpt.get_response() #ChatGPT產生的回答 the answers that ChatGPT gave
+    
+    update.message.reply_text(ai_reply_response) #用AI的文字回傳 reply the text that AI made
 def clear_handler(bot, update):
     """Clear message."""
     chatgpt.prompt.msg_list = []
-    chatgpt.prompt.messages = [
+    chatgpt.prompt.messages =  [
         {"role": "user", "content": context},
         {"role": "assistant", "content": f"{LANGUAGE_TABLE[chat_language]}"}
     ]
 
-    chatgpt.prompt.messagesTk = [50, 0]
+    chatgpt.prompt.messagesTk = [50,0]
     update.message.reply_text('cleared!')
 
 def start_handler(bot, update):
     update.message.reply_text('Hello! I am Chat-GPT, a chatbot powered by OpenAI GPT-3. Input your query and I will answer it.')
 
 # New a dispatcher for bot
-dispatcher = Dispatcher(bot, None)
+dispatcher = Dispatcher(bot,None)
 
 # Add handler for handling message, there are many kinds of message. For this handler, it particular handle text
 # message.
 dispatcher.add_handler(MessageHandler(Filters.text, reply_handler))
 # Add handler for /clear command
-dispatcher.add_handler(CommandHandler('clear', clear_handler))
+dispatcher.add_handler(CommandHandler('clear',clear_handler))
 
 # Add handler for /start command
-dispatcher.add_handler(CommandHandler('start', start_handler))
+dispatcher.add_handler(CommandHandler('start',start_handler))
 
 if __name__ == "__main__":
     # Running server
